a7d724e70689d8c82afb11044587f2aedddd3c46
qifan <353208262@qq.com>
2019-03-21 15:17:33 +0800

    add model and admin basic functions

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from . import models
 
 
 def index(request):
-    return render(request, 'blog/index.html', {'hello': 'hello,blog'})
+    article = models.Article.objects.get(pk=1)
+    return render(request, 'blog/index.html', {'article':article})
